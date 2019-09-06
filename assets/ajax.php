0d8fa39ea2760033db4bc9a14b5d60024ddef1d0
Lucas Pedro <lucasbarreiros2009@gmail.com>
2019-09-06 11:00:36 -0300

    002.1 - Ordem alfabética
    - Agora os resultados são organizados alfabeticamente

--- assets/ajax.php
+++ assets/ajax.php
@@ -5,22 +5,26 @@ if(isset($_GET['estado'])){
     $sql = $pdo->prepare("SELECT * FROM regiao_select WHERE estado = :estado");
     $sql->execute(['estado' => $estado]); 
     $query = $sql->fetchAll();
+    sort($query);
     echo json_encode($query);
 }elseif(isset($_GET['regiao'])){
     $regiao = $_GET['regiao'];
     $sql = $pdo->prepare("SELECT * FROM cidade_select where regiao = :regiao");
     $sql->execute(['regiao' => $regiao]);
     $query = $sql->fetchAll();
+    sort($query);
     echo json_encode($query);
 }elseif(isset($_GET['cidade'])){
     $cidade = $_GET['cidade'];
     $sql = $pdo->prepare("SELECT * FROM bairro_select where cidade = :cidade");
     $sql->execute(['cidade' => $cidade]);
     $query = $sql->fetchAll();
+    sort($query);
     echo json_encode($query);
 }else{
     $sql = $pdo->query("SELECT * FROM estado_select");
     $query = $sql->fetchAll();
+    sort($query);
     echo json_encode($query);
 }
 ?>
